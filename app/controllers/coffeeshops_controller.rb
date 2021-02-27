962fd19368a98f43a8cb5546ce72a498837a80d9
Trevor von Hake <tvonhake@outlook.com>
2021-02-26 17:01:25 -0700

    js files for coffeeshops CRUD, added simple css

--- app/controllers/coffeeshops_controller.rb
+++ app/controllers/coffeeshops_controller.rb
@@ -1,2 +1,32 @@
 class CoffeeshopsController < ApplicationController
+
+  def index
+    @coffeeshops = Coffeeshop.all
+    render component: "coffeeshops", props: {coffeeshops: @coffeeshops}
+  end
+
+  def show
+
+  end
+
+  def new
+    
+  end
+
+  def create
+
+  end
+
+  def edit
+
+  end
+
+  def update
+    
+  end
+
+  def delete
+
+  end
+
 end
---
+++ app/javascript/components/coffeeshops.js
@@ -0,0 +1,39 @@
+import React from 'react'
+
+const CoffeeShops = (props) =>{
+
+  const renderCoffeeShops = () =>{
+    return props.coffeeshops.map (coffeeshop => {
+      return(
+        <div className="item-container">
+          <div className="item-content">
+            <h2>{coffeeshop.name}</h2>
+            <p>Location: {coffeeshop.location}</p>
+            <p>Database ID: {coffeeshop.id}</p>
+            <hr className="itemSeparator"></hr>
+          </div>
+
+          {/* <a href={`/departments/${coffeeshop.id}/roasts`}>Open this Coffee Shop</a> */}
+          <a href={`/departments/${coffeeshop.id}/edit`}>Edit Coffee Shop Details</a>
+          <a href={`/departments/${coffeeshop.id}/`} data-method='delete'>Delete Coffee Shop</a>
+          
+        </div>
+      )
+    })
+
+  }
+
+  return(
+    <div className='main'>
+      <h1>All Coffee Shops </h1>
+      <div className='nav'>
+        <a href='/'>Home</a>
+        <a href='/coffeeshops/new'>Add New Coffee Shop</a>
+      </div>
+
+      {renderCoffeeShops()}
+    </div>
+  )
+}
+
+export default CoffeeShops
